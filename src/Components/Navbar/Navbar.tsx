9baefe10a051b60ed1bed0dcc1c0be88ea4501cd
Leonardoih <leonardoih090217@gmail.com>
2021-12-30 03:11:59 +0000

    nuevo cambio

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
         <div className="p-3">
             <nav className="navbar navbar-expand-md navbar-light bg-light   ">
         <div className="container-fluid">
-          <Link className="navbar-brand " to='/'>Navbar</Link>
+          <Link className="navbar-brand " to='/'>Navbar nuevo cambio</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
           </button>
